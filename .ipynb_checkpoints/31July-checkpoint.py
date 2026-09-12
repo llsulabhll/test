f3b379095a5117f3d888cf915be4ccf33a27a094
Abhasah <growwithtechsolutions@gmail.com>
2026-09-12 11:05:08 +0530

    message

---
+++ .ipynb_checkpoints/31July-checkpoint.py
@@ -0,0 +1,9 @@
+a=10
+b=20
+c=a+b
+print(c)
+
+perpendicular=10
+base=15
+1tri_area=(perpendicular*base)/2
+print("area of triangle is",1tri_area)
--- 31July.py
+++ 31July.py
@@ -1,3 +1,8 @@
+a=10
+b=20
+c=a+b
+print(c)
+
 perpendicular=10
 base=15
 1tri_area=(perpendicular*base)/2
